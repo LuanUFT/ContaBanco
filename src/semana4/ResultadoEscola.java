19074a117f232ce6aa84615bf21c410a9deb6151
Luan <porto.luan@mail.uft.edu.br>
2023-03-30 16:49:23 -0300

    atualizando

--- src/semana4/ResultadoEscola.java
+++ src/semana4/ResultadoEscola.java
@@ -1,11 +1,9 @@
 package semana4;
 
 public class ResultadoEscola{
-    public static void main (String[] args){
+    public static void main(String[] args) {
         int nota = 6;
-        if (nota >= 7)
-            System.out.println("Aprovado");
-        else
-            System.out.println("Reprovado");
+    String resultado = nota >= 7 ? "Aprovado": nota >= 5 && nota <7 ? "Recuperacao" : "Reprovado";
+    System.out.println(resultado);
     }
 }
